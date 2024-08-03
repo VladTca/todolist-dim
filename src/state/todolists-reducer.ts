be6d1be1dde6675d9d39ac0c1fd2fc7fb075a6c3
VladTca <votsats@gmail.com>
2024-08-03 18:53:41 +0200

    ⬇️ Downgrade dependencies: Fix typo in file name 'todolists_reducer.test.ts'.

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,10 +1,35 @@
-import {TodolistType} from "../App";
+import {FilterValuesType, TodolistType} from "../App";
 
-type ActionType = {
-    type: string
-    [key: string]: any
+// type ActionType = {
+//     type: string
+//     [key: string]: any
+//
+// }
 
+type RemoveTodolistActionType = {
+    type: "REMOVE-TODOLIST"
+    id: string
 }
+
+type AddTodolistActionType = {
+    type: "ADD-TODOLIST"
+    todolist: TodolistType
+}
+
+type ChangeTodolistTitleActionType = {
+    type: "CHANGE-TODOLIST-TITLE"
+    id: string
+    title: string
+}
+
+export type ChangeTodolistFilterActionType = {
+    type: "CHANGE-TODOLIST-FILTER"
+    id: string
+    filter: FilterValuesType
+}
+
+type ActionType = RemoveTodolistActionType | AddTodolistActionType
+    | ChangeTodolistTitleActionType | ChangeTodolistFilterActionType
 
 export const todolistsReducer = (state :TodolistType[], action:ActionType):TodolistType[] => {
     switch (action.type) {
@@ -20,4 +45,19 @@ export const todolistsReducer = (state :TodolistType[], action:ActionType):Todol
         default:
             throw new Error("Bad action type");
     }
+}
+
+export const RemoveTodolistAC = (id: string): RemoveTodolistActionType => {
+    return {type: "REMOVE-TODOLIST", id}
+}
+
+export const AddTodolistAC = (todolist: TodolistType): AddTodolistActionType => {
+    return {type: "ADD-TODOLIST", todolist}
+}
+
+export const ChangeTodolistTitleAC = (id: string, title: string): ChangeTodolistTitleActionType => {
+    return {type: "CHANGE-TODOLIST-TITLE", id, title}
+}
+export const  ChangeToloListFilterAC = (id: string, filter: FilterValuesType): ChangeTodolistFilterActionType => {
+    return {type: "CHANGE-TODOLIST-FILTER", id, filter}
 }
